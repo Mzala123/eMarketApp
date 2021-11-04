8c62b584de30c2fa450d2939ee7d28ba74ddc155
mtende <justicemwanzamj@gmai.com>
2021-11-04 04:09:45 +0200

    new changes

--- app_client/BuyerAccount/cart/cart.controller.js
+++ app_client/BuyerAccount/cart/cart.controller.js
@@ -13,30 +13,32 @@
 
         vm.formData = {};
         vm.formData.clientName = authentication.currentUser();
-        console.log(vm.formData.clientName.email);
-
-        var retrievedProducts = {
-          products: []
-        };
+       // console.log(vm.formData.clientName.email);
         var prods = JSON.parse(localStorage.getItem('cart_ids'));
         console.log("This is the cart section",prods);
-        console.log("PRODS",prods.productid.length);
-        for( var i=0; i <prods.productid.length; i++){
-          console.log("The products in the cart are" + prods.productid[i].productid)
-          product.findOneProduct(prods.productid[i].productid)
-            .then(function successCallback(response) {
-              var data = response.data;
-              vm.data = { products: data };
-              console.log("The products in the cart are", response.data);
+        console.log("PRODS",prods.products.length); 
+        //for( var i=0; i <prods.products.length; i++){
+              //console.log("The products in the cart are" + prods.productid[i].productid)
+              var data ;
+              var prods = JSON.parse(localStorage.getItem('cart_ids'));
+              vm.data = { products: prods };
+              console.log("The products in the cart are", prods);
               /*retrievedProducts.products.push({
                 "id":response.data[0].clientName
-              })*/
-              console.log("Pushed is ", retrievedProducts.products);
-            },
-              function errorCallback(response) {
-    
               })
-        }
+              // console.log("Pushed is ", retrievedProducts.products);   
+        //}
+      console.log("The product id is " + prods.products[0].productid)
+      product.findOneProduct(prods.products[0].productid)
+        .then(function successCallback(response) {
+          var data = response.data;
+          vm.data = { products: data };
+          console.log("The product to be edited is", response.data);
+        },
+          function errorCallback(response) {
+
+          })*/
+    
 
         
       }
--- app_client/BuyerAccount/home/home.controller.js
+++ app_client/BuyerAccount/home/home.controller.js
@@ -15,9 +15,11 @@
     vm.formData.clientName = authentication.currentUser();
     console.log(vm.formData.clientName.email);
 
-    var cartProducts = {
-      productid: []
-    };
+    /*var cartProducts = {
+      productid: [
+
+      ]
+    };*/
 
     product
       .findAllProducts()
@@ -34,15 +36,45 @@
 
     vm.addToCart = function (productid) {
       console.log("the product to be added to cart is " + productid);
-
-      cartProducts.productid.push({
-        productid
-      })
-     // console.log(cartProducts);
-      localStorage.setItem('cart_ids', JSON.stringify(cartProducts));
+      var name ="";
+      var price ="";
+      var description=""
+      var cartProducts ={
+        products: []
+      } ;
+      product.findOneProduct(productid)
+        .then(function successCallback(response) {
+          var data = response.data;
+          vm.data = { products: data };
+          name = response.data.name;
+          price = response.data.price;
+          description = response.data.description;
+          console.log("The product to be stored in localstorage is", response.data);
+          console.log("The product to be stored in localstorage is", name);
+          console.log("The product to be stored in localstorage is", price);
+           cartProducts.products.push({
+            productid: productid,
+            name: name,
+            price: price,
+            description: description
+          })
+          localStorage.setItem('cart_ids', JSON.stringify(cartProducts));
       var prods = JSON.parse(localStorage.getItem('cart_ids'));
       console.log("PRODS",prods);
-      console.log("PRODS",prods.productid[0].productid);
+      console.log("PRODS",prods.products[0].productid);
+        },
+          function errorCallback(response) {
+
+        })
+      /*cartProducts.productid.push({
+        productid
+      })*/
+      
+     // console.log(cartProducts);
+      /*localStorage.setItem('cart_ids', JSON.stringify(cartProducts));
+      var prods = JSON.parse(localStorage.getItem('cart_ids'));
+      console.log("PRODS",prods);
+      console.log("PRODS",prods.productid);*/
 
     }
 
